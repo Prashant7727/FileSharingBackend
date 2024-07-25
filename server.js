8521c531e3d475fc2b6f74ab93326508eef526df
Prashant7727 <prashant.sisodia08@gmail.com>
2024-07-25 20:22:35 +0530

    changes in server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/routes.js';
-import DBConnection from './database/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes.js";
+import DBConnection from "./database/db.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,14 +11,17 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to file-sharing app</h1>");
-  });
+  res.send("<h1>Welcome to file-sharing app</h1>");
+});
 DBConnection();
 
-
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`
+  );
+});
